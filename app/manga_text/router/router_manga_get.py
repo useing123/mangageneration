defcf24f02fdfa7a39ac1ecdc26199127404bb30
useing123 <useing@mail.ru>
2023-07-22 00:44:31 +0600

    fix router get

--- app/manga_text/router/router_manga_get.py
+++ app/manga_text/router/router_manga_get.py
@@ -5,13 +5,20 @@ from ..service import Service, get_service
 from ..adapters.jwt_service import JWTData
 from . import router
 
-
 class MangaGetResponse(AppModel):
     manga_id: str
     genre: str
     chapters_count: int
-    manga_chapters_story: list[str]
-#тут дополнить что бы возврашало другие параметры помимо тех что тут прописаны
+    user_id: str
+    title: str
+    chapters_title: list[str]
+    main_characters: str
+    funservice_characters: str
+    detailed_characters: str
+    manga_chapters_story: str
+    manga_frames_description: str
+    manga_story_dialogs: str
+    manga_images_description: str
 
 @router.get("/read/{manga_id}", response_model=MangaGetResponse)
 def get_manga(
@@ -28,7 +35,16 @@ def get_manga(
         manga_id=str(manga_data["_id"]),
         genre=manga_data["genre"],
         chapters_count=manga_data["chapters_count"],
-        manga_chapters_story=manga_data["manga_chapters_story"],
+        user_id=manga_data.get("user_id", ""),
+        title=manga_data.get("title", ""),
+        chapters_title=manga_data.get("chapters_title", []),
+        main_characters=manga_data.get("main_characters", ""),
+        funservice_characters=manga_data.get("funservice_characters", ""),
+        detailed_characters=manga_data.get("detailed_characters", ""),
+        manga_chapters_story=manga_data.get("manga_chapters_story", ""),
+        manga_frames_description=manga_data.get("manga_frames_description", ""),
+        manga_story_dialogs=manga_data.get("manga_story_dialogs", ""),
+        manga_images_description=manga_data.get("manga_images_description", "")
     )
 
     return manga
